941c0ee2b77d1ad91db829568ea843b7b8d4af8d
Mickael <micrandr@gmail.com>
2024-04-30 02:56:10 +0300

    W01 - Home performance update

---
+++ scripts/week02/scripts/bom.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('');
+
+const li = document.createElement('li');
+
+const deleteButton = document.createElement('button');
